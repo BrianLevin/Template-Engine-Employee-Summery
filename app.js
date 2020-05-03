c0bd7bcd3734fc338930ac19de5960cf4aa7b551
Brian Levin <bml201095@gmail.com>
2020-05-03 13:13:28 -0400

    created the create manager function which will run the code to make sure the manager will go first and created inquires to give the prompts and the manager and then created the .then function which will return the answers and then create the team

--- app.js
+++ app.js
@@ -8,3 +8,51 @@ const fs = require("fs");
 const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
+
+function createManager() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'Please enter your manager\s name: ',
+
+
+        },
+
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'what is your manager"s id?',
+
+
+
+        },
+
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'what is your manager"s email?',
+
+
+
+        },
+
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: 'what is your manager"s office number?',
+
+
+
+        },
+
+
+
+
+    ]).then(function (answers) {
+        var manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        createTeam();
+    });
+}
